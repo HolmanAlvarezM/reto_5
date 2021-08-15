efc263a85ee54ceb96249320c59d999fced412f9
Holman Alvarez <holman.alvarezm@gmail.com>
2021-08-15 13:48:07 -0500

    Delete warnings

--- src/main/java/mintic/App.java
+++ src/main/java/mintic/App.java
@@ -4,6 +4,6 @@ import mintic.view.ViewQueries;
 
 public class App {
     public static void main(String[] args) {
-        ViewQueries window = new ViewQueries();
+        new ViewQueries();
     }
 }
